61efadece0b12b42b5839b49bc1f3f7cdf5acb80
boybook <boybook@easecation.net>
2021-06-16 00:11:39 +0800

    NetEase

--- src/main/java/cn/nukkit/level/GlobalBlockPaletteInterface.java
+++ src/main/java/cn/nukkit/level/GlobalBlockPaletteInterface.java
@@ -58,4 +58,49 @@ public interface GlobalBlockPaletteInterface {
             return VALUES;
         }
     }
+
+    /*enum HardcodedVersion {
+        V1_16_100(419, false),
+        V1_16_200_NE(422, false),
+        V1_16_210(428, false),
+        V1_17(440, false),
+        ;
+
+        private static final HardcodedVersion[] VALUES = HardcodedVersion.values();
+        private static final Int2ObjectMap<HardcodedVersion[]> versionMap = new Int2ObjectOpenHashMap<>();
+
+        static {
+            for (HardcodedVersion version : VALUES) {
+                versionMap.putIfAbsent(version.protocol, new HardcodedVersion[2]);
+                if (!version.netease) {
+                    versionMap.get(version.protocol)[0] = version;
+                } else {
+                    versionMap.get(version.protocol)[1] = version;
+                }
+            }
+        }
+
+        private final int protocol;
+        private final boolean netease;
+
+        HardcodedVersion(int protocol, boolean netease) {
+            this.protocol = protocol;
+            this.netease = netease;
+        }
+
+        public int getProtocol() {
+            return this.protocol;
+        }
+
+        public static HardcodedVersion fromProtocol(int protocol, boolean netease) {
+            HardcodedVersion[] versions = versionMap.get(protocol);
+            if (versions == null) return VALUES[VALUES.length - 1];
+            if (!netease || versions[1] == null) return versions[0];
+            else return versions[1];
+        }
+
+        public static HardcodedVersion[] values0() {
+            return VALUES;
+        }
+    }*/
 }
