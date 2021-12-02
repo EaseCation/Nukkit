9ff7823c52e5a50ff8b83f1b7fb1d98a36bd2ad8
Reply123 <1871735932@qq.com>
2021-12-02 21:17:02 +0800

    canInteract

--- src/main/java/cn/nukkit/entity/Entity.java
+++ src/main/java/cn/nukkit/entity/Entity.java
@@ -2432,4 +2432,7 @@ public abstract class Entity extends Location implements Metadatable {
         hash = (int) (29 * hash + this.getId());
         return hash;
     }
+    public boolean canInteract(){
+        return true;
+    }
 }
