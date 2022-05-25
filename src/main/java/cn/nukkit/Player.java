7bedc305d9ce909c5abc0c43f5314cf33615ebfd
yuxuanchiadm <yuxuanchiadm@126.com>
2022-05-25 19:32:59 +0800

    修复传送和乘骑冲突的问题

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -2302,6 +2302,8 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                         this.forceMovement = new Vector3(this.x, this.y, this.z);
                     }
 
+                    if (riding != null) forceMovement = null;
+
                     if (this.forceMovement != null && (newPos.distanceSquared(this.forceMovement) > 0.1 || revert)) {
                         this.sendPosition(this.forceMovement, this.yaw, this.pitch, MovePlayerPacket.MODE_TELEPORT);
                     } else {
