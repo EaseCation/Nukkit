b860d9007d96532c07337400d3e5ff0cd9ba4a00
Woder <17339354+wode490390@users.noreply.github.com>
2022-09-10 23:54:48 +0800

    precondition for shared constants

--- src/main/java/cn/nukkit/SharedConstants.java
+++ src/main/java/cn/nukkit/SharedConstants.java
@@ -1,21 +1,24 @@
 package cn.nukkit;
 
+@SuppressWarnings("all")
 public final class SharedConstants {
+
+    public static final boolean PRODUCTION_ENVIRONMENT = false;
 
     // general
 
-    public static final boolean BREAKPOINT_DEBUGGING = true;
+    public static final boolean BREAKPOINT_DEBUGGING = !PRODUCTION_ENVIRONMENT && true;
 
-    public static final boolean ENABLE_TEST_COMMAND = true;
+    public static final boolean ENABLE_TEST_COMMAND = !PRODUCTION_ENVIRONMENT && true;
 
-    public static final boolean THREAD_SAFETY_CHECK = false;
+    public static final boolean THREAD_SAFETY_CHECK = !PRODUCTION_ENVIRONMENT && false;
 
-    public static final boolean NAN_INF_CHECK = false;
+    public static final boolean NAN_INF_CHECK = !PRODUCTION_ENVIRONMENT && false;
 
-    public static final boolean LOG_INVALID_BLOCK_AUX_ACCESS = true;
+    public static final boolean LOG_INVALID_BLOCK_AUX_ACCESS = !PRODUCTION_ENVIRONMENT && true;
 
-    public static final boolean EVENT_CREATE_STACK_TRACE = false;
-    public static final boolean EVENT_CANCEL_STACK_TRACE = false;
+    public static final boolean EVENT_CREATE_STACK_TRACE = !PRODUCTION_ENVIRONMENT && false;
+    public static final boolean EVENT_CANCEL_STACK_TRACE = !PRODUCTION_ENVIRONMENT && false;
 
     // chunk
 
@@ -26,7 +29,7 @@ public final class SharedConstants {
     public static final boolean ENABLE_SUB_CHUNK_NETWORK_OPTIMIZATION = true;
     public static final boolean ENABLE_EMPTY_SUB_CHUNK_NETWORK_OPTIMIZATION = true;
 
-    public static final boolean DUMP_NETWORK_SUB_CHUNK = false;
+    public static final boolean DUMP_NETWORK_SUB_CHUNK = !PRODUCTION_ENVIRONMENT && false;
 
     private SharedConstants() {
         throw new IllegalStateException();
