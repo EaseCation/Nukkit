279fcd3ce16e3f57e792e15be1a0a74796e44cdd
dxxxxdx <danshang@easecation.net>
2025-08-12 18:53:44 +0800

    烟花火箭在发射器内的逻辑修正

--- src/main/java/cn/nukkit/dispenser/FireworksDispenseBehavior.java
+++ src/main/java/cn/nukkit/dispenser/FireworksDispenseBehavior.java
@@ -3,7 +3,9 @@ package cn.nukkit.dispenser;
 import cn.nukkit.block.BlockDispenser;
 import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.item.EntityFirework;
+import cn.nukkit.entity.item.EntityItem;
 import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemFirework;
 import cn.nukkit.math.BlockFace;
 import cn.nukkit.math.Vector3;
 
@@ -13,11 +15,13 @@ public class FireworksDispenseBehavior extends DefaultDispenseBehavior {
     public Item dispense(BlockDispenser block, BlockFace face, Item item) {
         Vector3 dir = face.getUnitVector().asVector3();
         Vector3 pos = block.blockCenter().add(dir.multiply(0.5));
-
-        EntityFirework firework = new EntityFirework(block.level.getChunk(pos.getChunkX(), pos.getChunkZ()),
-                Entity.getDefaultNBT(pos, dir, (float) dir.yRotFromDirection(), (float) dir.xRotFromDirection()), true);
+        if (item instanceof ItemFirework) {
+            EntityFirework firework = ((ItemFirework) item).spawnFirework(block.getLevel(),pos,dir);
         firework.spawnToAll();
-
+        }else {
+            EntityFirework firework = new EntityFirework(block.level.getChunk(pos.getChunkX(), pos.getChunkZ()),
+                    Entity.getDefaultNBT(pos, dir, (float) dir.yRotFromDirection(), (float) dir.xRotFromDirection()), true);
+        }
         return null;
     }
 }
