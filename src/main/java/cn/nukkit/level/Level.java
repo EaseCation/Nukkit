5255494d5af3d3c98cf833b1b9335726d281c8c2
boybook <fangyizhou1997@qq.com>
2026-03-06 11:33:04 +0800

    修复Level.java中的玩家碰撞检测，解决卡方块问题，移除不必要的边界扩展逻辑

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -3222,7 +3222,8 @@ public class Level implements ChunkManager, Metadatable {
             if (player != null) {
                 Vector3 diff = player.getNextPosition().subtract(player.getPosition());
                 AxisAlignedBB bb = player.getBoundingBox().getOffsetBoundingBox(diff.x, diff.y, diff.z);
-                bb.expand(-0.01, -0.01, -0.01);
+                // 由于已经在SynapsePlayer116中，使用了this.newPosition = useItemData.playerPos.subtract(0, this.getBaseOffset(), 0); 所以能够准确使用客户端坐标计算
+                //bb.expand(-0.01, -0.01, -0.01);
                 if (boundingBox.intersectsWith(bb)) {
                     // This is a hack to prevent the player from placing blocks inside themselves
                     return LazyHolder.INVALID_ITEM;
